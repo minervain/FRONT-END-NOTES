7c855ce3e739d9b4bca9e119fc4ced1d81a0f84c
Ali Osman UÇAR <aliosmanucar987@gmail.com>
2023-04-26 13:36:27 +0300

    update

--- PatikaJS/Ödev2/js/js.js
+++ PatikaJS/Ödev2/js/js.js
@@ -1,27 +1,19 @@
-// Listeye boş karakter eklenemiyor. Bununla birlikte hiçbir şey yazılmadığında da aynı hatayı veriyor.
-// Yazacağınız JavaScript kodu içerisinde element eklemeyi sağlayan bir fonksiyon, element silmeyi sağlayan bir fonksiyon, yapıldı işaretlenmesini sağlayan bir fonksiyon olması gerekiyor.
-// Element eklendiğinde ve hata verirken sağ üstte uyarı verdiğini fark etmişsinizdir. Bunu sağlayan Bootstrap Toast bildirimdir. Sayfaya gidip toast nedir nasıl yapılır bunu araştırın ve kodunuza ekleyin.
-// Önce ana fonksiyonlar unutmayın. için uğraşın. Yapıldı, toast bildirim bunlar biraz daha için artistliği. Öncelikli amacını sağlıyor olması lazım.
-// Yazdığınız js dosyasını projenize eklemeyi
+//  Listeye boş karakter eklenemiyor. Bununla birlikte hiçbir şey yazılmadığında da aynı hatayı veriyor.
+//  Yazacağınız JavaScript kodu içerisinde element eklemeyi sağlayan bir fonksiyon, element silmeyi sağlayan bir fonksiyon, yapıldı işaretlenmesini sağlayan bir fonksiyon olması gerekiyor.
+//  Element eklendiğinde ve hata verirken sağ üstte uyarı verdiğini fark etmişsinizdir. Bunu sağlayan Bootstrap Toast bildirimdir. Sayfaya gidip toast nedir nasıl yapılır bunu araştırın ve kodunuza ekleyin.
+//  Önce ana fonksiyonlar unutmayın. için uğraşın. Yapıldı, toast bildirim bunlar biraz daha için artistliği. Öncelikli amacını sağlıyor olması lazım.
+//  Yazdığınız js dosyasını projenize eklemeyi
 
 
-let input=document.querySelector('#task')
-let ulDOM=document.querySelector('#list')
-// if (USER_NAME.value && SCORE.value) {
-//     addItem(USER_NAME.value, SCORE.value)  // gonderdikten sonra sifirladik
-//     USER_NAME.value = "" 
-//     SCORE.value = ""
-// } else {
-//     alertDOM.innerHTML = alertFunction(
-//         "Baslik Bilgisi",
-//         "Eksik Bilgi Girdiniz",
-//         "success"
-//     )
-// }
+let input=document.querySelector("#task")
+let ulDOM=document.querySelector("#list")
+const btn = document.querySelector("#liveToastBtn");
 
 
-input.addEventListener('submit', newElement)
 
+btn.addEventListener("submit",newElement)
+
+console.log(input.value)
 function newElement(e){
 
 if(input.value){
@@ -30,22 +22,21 @@ if(input.value){
 }
 
 else{
-    alertDOM.innerHTML = alertFunction(
-                "Baslik Bilgisi",      "Eksik Bilgi Girdiniz",
-                "success"
-             )
+    // alertDOM.innerHTML = alertFunction(
+    //             "Baslik Bilgisi",      "Eksik Bilgi Girdiniz",
+    //             "success"
+    //          )
          }
 
 
 }
 
-console.log(input)
 
  function addItem(gelendeger){
     let liDOM = document.createElement('li')
     liDOM.innerHTML=`${gelendeger}`
     liDOM.className = "list-group-item list-group-item-secondary";
 
-    ulDOM.append(liDOM)
+    ulDOM.appendChild(liDOM)
 
  }
