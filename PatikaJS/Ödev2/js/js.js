39ef7b0512bc7be8ac725d500b7989d78bcf96e9
Ali Osman UÇAR <aliosmanucar987@gmail.com>
2023-04-26 14:53:53 +0300

    Checked

--- PatikaJS/Ödev2/js/js.js
+++ PatikaJS/Ödev2/js/js.js
@@ -2,16 +2,18 @@
 //  Yazacağınız JavaScript kodu içerisinde element eklemeyi sağlayan bir fonksiyon, element silmeyi sağlayan bir fonksiyon, yapıldı işaretlenmesini sağlayan bir fonksiyon olması gerekiyor.
 //  Element eklendiğinde ve hata verirken sağ üstte uyarı verdiğini fark etmişsinizdir. Bunu sağlayan Bootstrap Toast bildirimdir. Sayfaya gidip toast nedir nasıl yapılır bunu araştırın ve kodunuza ekleyin.
 //  Önce ana fonksiyonlar unutmayın. için uğraşın. Yapıldı, toast bildirim bunlar biraz daha için artistliği. Öncelikli amacını sağlıyor olması lazım.
-//  Yazdığınız js dosyasını projenize eklemeyi
+
 
 
 let input=document.querySelector("#task")
 let ulDOM=document.querySelector("#list")
-const btn = document.querySelector("#liveToastBtn");
+let focs = document.querySelector('#liveToastBtn')
 
 
 
-btn.addEventListener("submit",newElement)
+
+input.addEventListener("submit",newElement)
+ulDOM.addEventListener("click", listClick);
 
 console.log(input.value)
 function newElement(e){
@@ -36,7 +38,18 @@ else{
     let liDOM = document.createElement('li')
     liDOM.innerHTML=`${gelendeger}`
     liDOM.className = "list-group-item list-group-item-secondary";
+    const a = document.createElement("a");
+    a.classList = "delete-item float-right";
+    a.setAttribute("href", "#");
+    a.innerHTML = '<i class="fas fa-times"></i>';
 
-    ulDOM.appendChild(liDOM)
+
+    ulDOM.prepend(liDOM)
 
  }
+
+ function listClick(e) {
+    if (e.target.tagName == "LI") {
+      e.target.classList.toggle("checked");
+    }
+  }
