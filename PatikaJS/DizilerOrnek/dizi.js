c30b3664c8a49a279ab84698d28c3468ff0a3fbc
Ali Osman UÇAR <aliosmanucar987@gmail.com>
2023-04-28 21:50:20 +0300

    MF

--- PatikaJS/DizilerOrnek/dizi.js
+++ PatikaJS/DizilerOrnek/dizi.js
@@ -59,3 +59,55 @@ console.log(allUsers.join(" --- "))
 allUsers.splice(allUsers.length -1, 0, "Melissa")
 allUsers.splice(Math.floor( allUsers.length / 2 ), 0, "Lorem")
 console.log(allUsers)
+
+/*FİLTER MAP KULLANIMI*/
+
+// array filter kullanimi
+// 5 harften fazla olanlar (6) ??
+const PRODUCTS = ["Mic", "Cable", "Speaker", "Desktop PC", "Server", "Mouse", "Keyboard"]
+
+const NEW_PRODUCTS = PRODUCTS.filter(item => item.length > 5)
+console.log(NEW_PRODUCTS)
+
+
+// aktif kullanicilar ??
+const USERS = [
+  {fullName: "Ayse Sumer", isActive: false},
+  {fullName: "Ahmet Urgan", isActive: true},
+  {fullName: "Asya Basar", isActive: true},
+  {fullName: "Aksel Durmaz", isActive: false},
+]
+
+// const ACTIVE_USERS = USERS.filter(user => user.isActive === true)
+const ACTIVE_USERS = USERS.filter(user => user.isActive )
+console.log(ACTIVE_USERS)
+
+
+
+// array map kullanimi
+// https://developer.mozilla.org/tr/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+const USERS1 = ["AYSE", "MehMet", "TugCE", "AkSEL"]
+
+// userName icinde orjinal isim kalsin, 
+// shortName icinde ilk harf buyuk . (A.)
+// newName icinde ilk harf buyuk olsun
+// {userName: "AYSE", shortName: "A.", newName: "Ayse"}
+
+const NEW_USERS = USERS1.map( user => user.toLowerCase() )
+console.log(NEW_USERS)
+
+// const USERS_OBJ = USERS.map( item =>
+//   {
+//     return {
+//       userName: item, shortName: `${item[0]}.`, newName: `${item[0].toUpperCase()}${item.slice(1).toLowerCase()}`
+//     }
+//   } 
+// )
+
+const USERS_OBJ = USERS1.map( item => (
+      {userName: item, shortName: `${item[0]}.`, newName: `${item[0].toUpperCase()}${item.slice(1).toLowerCase()}`}
+    )
+)
+
+console.log(USERS_OBJ)
